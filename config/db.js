2ffe3302a6e1eaa72bf01570e268c2dbefa6057f
GenericDevGeorgia <genericdevgeorgia@Mac.attlocal.net>
2025-01-10 14:10:16 -0500

    validations

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-      await mongoose.connect('mongodb+srv://testUser:eQKQHtYpoE0nQlbs@cluster0.jehzz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'); 
+      await mongoose.connect(process.env.DB_URI); 
       console.log('MongoDB Connected...');
   } catch (err) {
       console.error('Error connecting to MongoDB:', err.message);
@@ -13,55 +13,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-
-// "use strict";
-// const MongoClient = require('mongodb').MongoClient;
-// const MONGODB_URI = process.env.DB_URI; // or Atlas connection string
-// let cachedDb = null;
-
-// function connectToDatabase(uri) {
-// 	console.log('=> connect to database');
-// 	if (cachedDb) {
-// 		console.log('=> using cached database instance');
-// 		return Promise.resolve(cachedDb);
-// 	}
-// 	return MongoClient.connect(uri)
-// 		.then(db => {
-// 			cachedDb = db; //For mongo client before v3
-// 			cachedDb = db.db("items"); //For mongo client v3,item is db i creted
-// 			return cachedDb;
-// 		});
-// }
-
-// function queryDatabase(db) {
-// 	console.log('=> query database');
-// 	return db.collection('fruits').find({}).toArray()
-// 		.then(() => {
-// 			return {
-// 				statusCode: 200,
-// 				body: 'success'
-// 			};
-// 		})
-// 		.catch(err => {
-// 			console.log('=> an error occurred: ', err);
-// 			return {
-// 				statusCode: 500,
-// 				body: 'error'
-// 			};
-// 		});
-// }
-// module.exports.handler = (event, context, callback) => {
-// 	context.callbackWaitsForEmptyEventLoop = false;
-// 	console.log('event: ', event);
-// 	connectToDatabase(MONGODB_URI)
-// 		.then(db => queryDatabase(db))
-// 		.then(result => {
-// 			console.log('=> returning result: ', result);
-// 			callback(null, result);
-// 		})
-// 		.catch(err => {
-// 			console.log('=> an error occurred: ', err);
-// 			callback(err);
-// 		});
-// };
